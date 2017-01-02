b92aba3ccda7395bbb4c6e63942c67b9a3bdd46d
Nik <nikita.kudinov@gmail.com>
2017-01-02 20:10:19 +0100

    Tags component wrapping Tag to build a list of tags

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,6 +8,7 @@ function loadStories() {
   require('../lib/components/Input/stories');
   require('../lib/components/CheckboxGroup/stories');
   require('../lib/components/Tag/stories');
+  require('../lib/components/Tags/stories');
 }
 
 configure(loadStories, module);
