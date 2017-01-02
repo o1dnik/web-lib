335634bd42588e9b972becbff608b5a1e7767d6c
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-01-02 13:56:55 +0100

    add loader

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ import '../lib/scss/main.scss';
 function loadStories() {
   require('../lib/components/Button/stories');
   require('../lib/components/Box/stories');
+  require('../lib/components/Loader/stories');
 }
 
 configure(loadStories, module);
