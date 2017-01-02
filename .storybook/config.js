f3b48e5f969ba5bf8309c02a612c1d4c4d40db95
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-01-02 13:40:12 +0100

    add box component

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import '../lib/scss/main.scss';
 
 function loadStories() {
   require('../lib/components/Button/stories');
+  require('../lib/components/Box/stories');
 }
 
 configure(loadStories, module);
