38ba2ebf27d0d014f84b8e178a92e0beb47c14b0
Nik <nikita.kudinov@gmail.com>
2017-01-03 18:19:17 +0100

    Select component with storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,6 +9,7 @@ function loadStories() {
   require('../lib/components/CheckboxGroup/stories');
   require('../lib/components/Tag/stories');
   require('../lib/components/Tags/stories');
+  require('../lib/components/Select/stories');
 }
 
 configure(loadStories, module);
