3a74999a935a115b14def2c2627889788b4f760f
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-03-21 14:13:27 +0100

    add error

--- src/server-errors-map.js
+++ src/server-errors-map.js
@@ -6,6 +6,7 @@ const serverErrorsMap = {
   'default_error': DEFAULT_ERROR,
   'unknown_code': DEFAULT_ERROR,
   'bad_request': DEFAULT_ERROR,
+  'server_error': DEFAULT_ERROR,
   'account_already_confirmed': 'This account is already confirmed',
   'wrong_credentials': 'Sorry, but these crediantials are not correct.',
   'invalid_data': 'Sorry, an error has occurred. Please check your input.',
