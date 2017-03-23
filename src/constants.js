59241b5e94a9301e819a036f613287b3793f138d
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-03-23 09:23:34 +0100

    add referral tracking const

--- src/constants.js
+++ src/constants.js
@@ -28,6 +28,7 @@ export const COMPANIES = 'COMPANIES';
 export const ACCOUNT = 'ACCOUNT';
 export const PROFILE = 'PROFILE';
 export const SOCIAL_PLATFORM = 'SOCIAL_PLATFORM';
+export const REFERRAL = 'REFERRAL';
 
 export const JOBS_BY_COMPANY_ID = 'JOBS_BY_COMPANY_ID';
 export const JOB = 'JOB';
