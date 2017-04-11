9ce3a431f02c1b1e693d2dae59d0effdb23baee9
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-04-11 12:27:32 +0200

    add notifications component

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {NotificationStack} from 'react-notification';
+import {hideNotification} from '../../actions/notifications-actions';
+
+class Notifications extends Component {
+
+  handleDismiss = (notification) => {
+    this.props.hideNotification(notification);
+  }
+
+  render() {
+    const {notifications} = this.props;
+    if (!notifications.length) return null;
+
+    return (
+      <NotificationStack
+        notifications={
+          notifications
+            .map(n => ({...n, className: 'notification-bar-custom'}))
+        }
+        onDismiss={this.handleDismiss}
+      />
+    );
+  }
+
+}
+
+export default connect(({notifications}) => ({
+  notifications: notifications.entities
+}), {hideNotification})(Notifications);
