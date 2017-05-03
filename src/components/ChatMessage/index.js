0d7c3963bee6902c19a607405cde3da40bb914ad
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-05-03 15:25:12 +0200

    split chat message by new line

--- src/components/ChatMessage/index.js
+++ src/components/ChatMessage/index.js
@@ -43,7 +43,7 @@ class Message extends Component {
           </div>
         </div>
         <div className='message-text'>
-          {text}
+          {text.split(/\r\n|\r|\n/g).map(chunk => <p key={chunk}>{chunk}</p>)}
         </div>
       </div>
     )
--- src/components/ChatMessage/stories.js
+++ src/components/ChatMessage/stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import Message from './index'
 
-const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus fugiat iste labore numquam odit officia provident rem tempore veniam. Culpa dolorum, eum facere maxime porro quis sint. Dicta eaque, ex facilis ipsum provident quod sint suscipit. Accusantium ad aspernatur corporis culpa cupiditate eveniet excepturi fugiat impedit inventore labore, libero mollitia nesciunt obcaecati, perspiciatis qui quo sapiente sequi sint soluta tenetur ullam unde vel velit? A accusantium amet animi aspernatur aut, beatae commodi consequatur corporis debitis dicta distinctio ducimus earum, eius est hic illum in iure magni nam natus nesciunt nisi officiis optio, quam quidem saepe sequi sint velit voluptas voluptatum!'
+const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\nDoloribus fugiat iste labore numquam odit officia provident rem tempore veniam.\rCulpa dolorum, eum facere maxime porro quis sint. Dicta eaque, ex facilis ipsum provident quod sint suscipit.\r\nAccusantium ad aspernatur corporis culpa cupiditate eveniet excepturi fugiat impedit inventore labore, libero mollitia nesciunt obcaecati, perspiciatis qui quo sapiente sequi sint soluta tenetur ullam unde vel velit? A accusantium amet animi aspernatur aut, beatae commodi consequatur corporis debitis dicta distinctio ducimus earum, eius est hic illum in iure magni nam natus nesciunt nisi officiis optio, quam quidem saepe sequi sint velit voluptas voluptatum!'
 storiesOf('Message', module)
   .add('Default', () => (
     <div style={{backgroundColor: '#f1f1f1', padding: '30px'}}>
