e8ee90a5a384fa82f1c778b662ecefb777f1a0d1
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-05-30 11:38:12 +0200

    improve examples

--- src/components/Tooltip/stories.js
+++ src/components/Tooltip/stories.js
@@ -6,10 +6,37 @@ import Tag from '../Tag'
 storiesOf('Tooltip', module)
   .add('Default', () => (
     <div style={{padding: '50px'}}>
-      <Tag color='primary' data-tip='hello world'>Tooltip</Tag>
-      <Tooltip effect='solid' />
 
-      <Tag data-tip='hello world2'>Tooltip</Tag>
-      <Tooltip effect='solid' />
+      <Tag
+        color='primary'
+        data-for='main'
+        data-tip
+      >
+        Tooltip
+      </Tag>
+
+      <Tooltip id='main' effect='solid'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>
+          At dolore dolores eius obcaecati porro, quos tempore unde vero
+          voluptates voluptatibus?
+        </p>
+      </Tooltip>
+
+      <Tag data-for='second' data-tip>
+        Tooltip 2
+      </Tag>
+
+      <Tooltip id='second' effect='solid' type='light'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>
+          At dolore dolores eius obcaecati porro, quos tempore unde vero
+          voluptates voluptatibus?
+        </p>
+        <p>
+          At dolore dolores eius obcaecati porro, quos tempore unde vero
+          voluptates voluptatibus?
+        </p>
+      </Tooltip>
     </div>
   ))
