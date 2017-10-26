d5218a09f569d66e16f12a299f9cb30c24c0ed03
Dmitrii Solovev <dimonnwc3@gmail.com>
2017-10-26 15:35:34 +0200

    add erorr codes

--- src/server-errors-map.js
+++ src/server-errors-map.js
@@ -14,17 +14,24 @@ const serverErrorsMap = {
   'invalid_token': 'Session expired. Please login.', // JWT fails
   'token_invalid': ( // email confirmation token fails
     <span>
-      Sorry, but this code is expired or invalid. <Link to='/resend'>Resend E-mail.</Link>
+      Sorry, but this code is expired or invalid. <Link to='/resend'>Resend confirmation E-mail.</Link>
     </span>
   ),
   'email_not_found': 'Sorry, but we do not have an account with this email address.',
   'email_not_unique': 'Sorry, but we already have an account with this email address.',
   'email_permission_missing': 'Please visit settings of your social network to allow usage of your Email for your MoBerries registration.',
   'account_not_confirmed': 'The E-mail was already used. Please check your inbox.',
-  'account_inactive': 'The E-mail was already used. Please check your inbox.',
+  'account_inactive': (
+    <span>
+      This account is not activated.&nbsp;
+      Please check your email or order a new confirmation code.&nbsp;
+      <Link to='/resend'>here</Link>
+    </span>
+  ),
   'auth_failed': 'Authentication failed.',
   'account_exists': 'User with this E-mail already exists',
   'wrong_format': 'Wrong format',
+  'social_account_exists': 'Sorry, but this social account is already connected with another MoBerries account',
   'not_confirmed': (
     <span>
       This email address is not yet confirmed.&nbsp;
@@ -34,12 +41,12 @@ const serverErrorsMap = {
   'candidate_inactive': 'Candidate disactivated his profile',
   'candidate_not_found': 'Cannot update your account',
   'not_completed': 'Please complete the sign-up form',
-  'account_deleted': 'The account has been deleted !',
+  'account_deleted': 'This account is suspended and is going to be deleted within next 24 hours',
   'job_not_found': 'Job not found !',
   'job_inactive': 'Job has been disactivated !',
   'wrong_password': 'Sorry, but this password is not correct.',
   'authentication_failed': 'There was a problem with your authentication. Please, try to login again',
-  'not_found': 'Not found.'
+  'not_found': DEFAULT_ERROR
 }
 
 export default serverErrorsMap
