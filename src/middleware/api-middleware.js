3361eacc7b56c27f29ea830e98b1c094a96023a4
Dmitrii Solovev <dimonnwc3@gmail.com>
2018-01-11 14:29:18 +0100

    move error handling to middleware

--- src/middleware/api-middleware.js
+++ src/middleware/api-middleware.js
@@ -9,8 +9,7 @@ import {
   UPDATE,
   DELETE,
   CREATE,
-  PATCH,
-  SESSION_EXPIRED
+  PATCH
 } from "../constants"
 
 export default axios => () => next => action => {
@@ -62,73 +61,7 @@ export default axios => () => next => action => {
       return res
     })
     .catch(err => {
-      if (err.response) {
-        if (err.response.status >= 500) {
-          if (!err.response.data) {
-            err.response.data = { code: "server_error" }
-          }
-
-          if (!err.response.data.code) {
-            err.response.data.code = "server_error"
-          }
-        }
-
-        if (err.response.status < 500 && err.response.status >= 400) {
-          if (err.response.status === 401 || err.response.status === 403) {
-            next({ type: getActionType(SESSION_EXPIRED) })
-
-            if (err.response.status === 401) {
-              if (!err.response.data) {
-                err.response.data = { code: "authentication_failed" }
-              }
-
-              if (!err.response.data.code) {
-                err.response.data.code = "authentication_failed"
-              }
-            } else if (err.response.status === 403) {
-              if (!err.response.data) {
-                err.response.data = { code: "authorization_failed" }
-              }
-
-              if (!err.response.data.code) {
-                err.response.data.code = "authorization_failed"
-              }
-            }
-          } else if (err.response.status === 404) {
-            if (!err.response.data) {
-              err.response.data = { code: "not_found" }
-            }
-
-            if (!err.response.data.code) {
-              err.response.data.code = "not_found"
-            }
-          } else {
-            if (!err.response.data) {
-              err.response.data = { code: "bad_request" }
-            }
-
-            if (!err.response.data.code) {
-              err.response.data.code = "bad_request"
-            }
-          }
-        }
-      } else if (err.request) {
-        if ("onLine" in window.navigator) {
-          const { onLine } = window.navigator
-
-          if (onLine) {
-            err.response = { data: { code: "default_error", onLine } }
-          } else {
-            err.response = { data: { code: "network_error", onLine } }
-          }
-        } else {
-          err.response = { data: { code: "network_error", onLine: "unknown" } }
-        }
-      } else {
-        err.response = { data: { code: "default_error" } }
-      }
-
-      next({ type: getActionType(type, FAIL), err: err.response, ...rest })
+      next({ type: getActionType(type, FAIL), err, ...rest, apiError: true })
       throw err
     })
 }
