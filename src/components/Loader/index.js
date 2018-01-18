ddb1ecb90540073c61e4fe5d52756768f0a34128
Dmitrii Solovev <dimonnwc3@gmail.com>
2018-01-18 10:47:19 +0100

    change viewbox

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -15,7 +15,7 @@ const Loader = props => {
       className="loader-container"
       width={sizeMap[props.size]}
       height={sizeMap[props.size]}
-      viewBox="0 0 122 122"
+      viewBox="0 0 120 120"
     >
       <g
         className="loader"
